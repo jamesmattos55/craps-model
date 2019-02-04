ed9bf6ba4f6c0a22dbc0abcab645953c81f95f81
James Mattos <jamesmattos55@yahoo.com>
2019-02-04 14:53:24 -0700

    Added javadoc comments and generated html.

--- src/edu/cnm/deepdive/craps/model/Game.java
+++ src/edu/cnm/deepdive/craps/model/Game.java
@@ -5,6 +5,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ *
+ * <code>Game</code> implements a simple play engine for the main roll/bet of a
+ * game of Craps. Additionally, it can be used to keep tallies across several
+ * rounds of play. Side bets are not supported.
+ *
+ * @author James Mattos &amp; Deep Dive Coding Java + Android cohort 6
+ * @version 1.0
+ *
+ */
 public class Game {
 
   private int pointValue;
@@ -14,10 +24,24 @@ public class Game {
   private long wins;
   private long losses;
 
+  /**
+   * Initializes this instance with the specified source of randomness.
+   *
+   * @param rng    source of randomness.
+   */
+
   public Game(Random rng) {
     this.rng = rng;
     rolls = new LinkedList<>();
   }
+
+  /**
+   * Prepares this instance tp play a round&mdash;i.e. returns the game state to
+   * {@link State#COME_OUT}, clears the established point, and clears the lis of
+   * rolls. This method <strong>must</strong> be invoked prior to {@link
+   * #play()}, even immediately after creating and initializing the instance via
+   * {@link #Game(Random)}.
+   */
 
   public void reset() {
     state = State.COME_OUT;
@@ -38,6 +62,15 @@ public class Game {
     rolls.add(diceRoll);
   }
 
+  /**
+   * Plays one round of Craps, from come-out roll to win or loss. if this is invoked
+   * on an instance already in a terminal state ({@link State#WIN} or
+   * {@link State#LOSS}), no exception is thrown, but the state of this
+   * <code>Game</code> instance doesn't change.
+   *
+   * @return terminal state ({@link State#WIN} or {@link State#LOSS}) of game.
+   */
+
   public State play() {
     while (state != State.WIN && state != State.LOSS) {
       roll();
@@ -51,13 +84,33 @@ public class Game {
     return state;
   }
 
+  /**
+   * returns established point.
+   *
+   * @return
+   */
+
   public int getPointValue() {
     return pointValue;
   }
 
+  /**
+   * Returns current state of game. This will be the {@link State#COME_OUT}
+   * state after {@link #reset()} is invoked, and a terminal state ({@link
+   * State#WIN} or {@link State#LOSS}) after {@link #play()} is invoked.
+   *
+   * @return
+   */
+
   public State getState() {
     return state;
   }
+
+  /**
+   * Constructs and returns a deep (safe) copy of the list of rolls in the current game.
+   *
+   * @return
+   */
 
   public List<int[]> getRolls() {
     List<int[]> copy = new LinkedList<>();
@@ -67,9 +120,22 @@ public class Game {
     return copy;
   }
 
+  /**
+   * Returns the tally of wins recorded since this instance was created
+   * and initialized
+   *
+   * @return
+   */
+
   public long getWins() {
     return wins;
   }
+
+  /**
+   *  Returns the tally of wins recorded since this instance was created
+   *  and initialized
+   * @return
+   */
 
   public long getLosses() {
     return losses;
--- src/edu/cnm/deepdive/craps/model/State.java
+++ src/edu/cnm/deepdive/craps/model/State.java
@@ -1,5 +1,14 @@
 package edu.cnm.deepdive.craps.model;
 
+/**
+ * <code>State</code> implements a basic state machine for the main play of a
+ * game of craps, starting with the come-out roll, and ending with a win or loss
+ * of the main bet.
+ *
+ * @author James Mattos
+ * @version 1.0
+ */
+
 public enum State {
   COME_OUT {
     @Override
@@ -32,6 +41,15 @@ public enum State {
   WIN,
   LOSS;
 
+  /**
+   * Applies the specified roll sum to this state , returing the same state
+   * resulting from the transition represented by the roll. For the terminal
+   * states ({@link #WIN} and {@link #LOSS}), no change of state will result from
+   *
+   * @param roll
+   * @param pointValue
+   * @return
+   */
 
   public State change(int roll, int pointValue) {
     return this;
